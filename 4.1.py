e0c4ca84b1ec1dcd2f763ba22dead3a3f0c0c1fc
Olesya <lesionok81@mail.ru>
2022-12-17 21:33:17 +0500

    pi

--- 4.1.py
+++ 4.1.py
@@ -2,20 +2,23 @@
 # *Пример:*
 #
 # - при d = 0.0001, π = 3.1415.    10^-1 ≤ d ≤ 10^-10
-
-
-# from math import pi
-#
-# d =  input("Введите число для заданной точности числа Пи:\n")
-# d = string
-#
-# print(f'число Пи с заданной точностью {d} равно {round(pi, d)}')
-
+import math
 from math import pi
 
-d = int(input("Введите число d:\n"))
+from unicodedata import decimal
+
+d = float(input("Введите число d:\n"))
 
 print(pi)
 
-template = '{:.' + str(d) + 'f}'
-print(template.format(pi))
+i = str(d)
+a = abs(i.find('.') - len(i)) - 1
+print(a)
+
+s = round(math.pi, a)
+
+print(f'число Пи с заданной точностью {d} равно {s}')
+
+
+
+
